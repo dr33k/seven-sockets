58606bb4b98f0cc3152c1d96e760b4a9341c0228
EmelieC <emeliec@hardcorebiometric.com>
2026-03-16 16:38:45 +0100

    Initial commit

---
+++ src/main/java/com/seven/seven_sockets/Application.java
@@ -0,0 +1,13 @@
+package com.seven.seven_sockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
